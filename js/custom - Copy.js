8394b8625a3572cdb8ec7ef27979f3d78bba2feb
Mihir Sojitra <76615361+MihirSojitra24@users.noreply.github.com>
2021-11-25 16:07:58 +0530

    remove extra

--- js/custom - Copy.js
+++ js/custom - Copy.js
@@ -208,61 +208,151 @@ setTimeout(function() {
   });
 }, 3000);
 
+// Scroll Section 
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".panel");
+
+let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none", // <-- IMPORTANT!
+    scrollTrigger: {
+      trigger: ".myDetails_wrapper",
+      pin: true,
+       markers: false,
+      scrub:1,
+      //snap: directionalSnap(1 / (sections.length - 1)),
+      end: "+=3000"
+    }
+  });
+
+gsap.set(".box-1, .box-2", {y: 100});
+// gsap.set(".col1_animate, .col2_animate, .col3_animate, .col4_animate, .col5_animate, .col6_animate, .col7_animate", {y: 100});
+ScrollTrigger.defaults({markers: false});
+
+// red section
+gsap.to(".box-1", {
+  y: -130,
+  duration: 2,
+  ease: "elastic",
+  scrollTrigger: {
+    trigger: ".box-1",
+    containerAnimation: scrollTween,
+    start: "left center",
+    toggleActions: "play none none reset",
+    id: "1",
+  }
+});
+
+
+// gray section
+gsap.to(".box-2", {
+  y: -120,
+  // backgroundColor: "#1e90ff",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".box-2",
+    containerAnimation: scrollTween,
+    start: "center bottom",
+    end: "center center",
+    scrub: true,
+    id: "2"
+  }
+});
+
+// purple section
+// ScrollTrigger.create({
+//   trigger: ".box-3",
+//   containerAnimation: scrollTween,
+//   toggleClass: "active",
+//   start: "center 20%",
+//   end: "center 80%",
+//   id: "3"
+// });
+
+// green section
+ScrollTrigger.create({
+  trigger: ".green",
+  containerAnimation: scrollTween,
+  start: "center 65%",
+  end: "center 51%",
+  onEnter: () => console.log("enter"),
+  onLeave: () => console.log("leave"),
+  onEnterBack: () => console.log("enterBack"),
+  onLeaveBack: () => console.log("leaveBack"),
+  onToggle: self => console.log("active", self.isActive),
+  id: "4"
+});
+
+
+
+// helper function for causing the sections to always snap in the direction of the scroll (next section) rather than whichever section is "closest" when scrolling stops.
+// function directionalSnap(increment) {
+//   let snapFunc = gsap.utils.snap(increment);
+//   return (raw, self) => {
+//     let n = snapFunc(raw);
+//     return Math.abs(n - raw) < 1e-4 || (n < raw) === self.direction < 0 ? n : self.direction < 0 ? n - increment : n + increment;
+//   };
+// }
+
+// making the code pretty/formatted.
+// PR.prettyPrint();
+
 
 // Timeline Sec JS
-// (function($) {
-//   $.fn.timeline = function() {
-//     var selectors = {
-//       id: $(this),
-//       item: $(this).find(".timeline-item"),
-//       activeClass: "timeline-item--active",
-//       img: ".timeline__img"
-//     };
-//     selectors.item.eq(0).addClass(selectors.activeClass);
-//     selectors.id.css(
-//       "background-image",
-//       "url(" +
-//         selectors.item
-//           .first()
-//           .find(selectors.img)
-//           .attr("src") +
-//         ")"
-//     );
-//     var itemLength = selectors.item.length;
-//     $(window).scroll(function() {
-//       var max, min;
-//       var pos = $(this).scrollTop();
-//       selectors.item.each(function(i) {
-//         min = $(this).offset().top;
-//         max = $(this).height() + $(this).offset().top;
-//         var that = $(this);
-//         if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
-//           selectors.item.removeClass(selectors.activeClass);
-//           selectors.id.css(
-//             "background-image",
-//             "url(" +
-//               selectors.item
-//                 .last()
-//                 .find(selectors.img)
-//                 .attr("src") +
-//               ")"
-//           );
-//           selectors.item.last().addClass(selectors.activeClass);
-//         } else if (pos <= max - 40 && pos >= min) {
-//           selectors.id.css(
-//             "background-image",
-//             "url(" +
-//               $(this)
-//                 .find(selectors.img)
-//                 .attr("src") +
-//               ")"
-//           );
-//           selectors.item.removeClass(selectors.activeClass);
-//           $(this).addClass(selectors.activeClass);
-//         }
-//       });
-//     });
-//   };
-// })(jQuery);
+(function($) {
+  $.fn.timeline = function() {
+    var selectors = {
+      id: $(this),
+      item: $(this).find(".timeline-item"),
+      activeClass: "timeline-item--active",
+      img: ".timeline__img"
+    };
+    selectors.item.eq(0).addClass(selectors.activeClass);
+    selectors.id.css(
+      "background-image",
+      "url(" +
+        selectors.item
+          .first()
+          .find(selectors.img)
+          .attr("src") +
+        ")"
+    );
+    var itemLength = selectors.item.length;
+    $(window).scroll(function() {
+      var max, min;
+      var pos = $(this).scrollTop();
+      selectors.item.each(function(i) {
+        min = $(this).offset().top;
+        max = $(this).height() + $(this).offset().top;
+        var that = $(this);
+        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+          selectors.item.removeClass(selectors.activeClass);
+          selectors.id.css(
+            "background-image",
+            "url(" +
+              selectors.item
+                .last()
+                .find(selectors.img)
+                .attr("src") +
+              ")"
+          );
+          selectors.item.last().addClass(selectors.activeClass);
+        } else if (pos <= max - 40 && pos >= min) {
+          selectors.id.css(
+            "background-image",
+            "url(" +
+              $(this)
+                .find(selectors.img)
+                .attr("src") +
+              ")"
+          );
+          selectors.item.removeClass(selectors.activeClass);
+          $(this).addClass(selectors.activeClass);
+        }
+      });
+    });
+  };
+})(jQuery);
 
-// $("#timeline-1").timeline();
+$("#timeline-1").timeline();
